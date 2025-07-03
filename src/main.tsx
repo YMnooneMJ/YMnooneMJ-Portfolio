ba6bdf9357ad76322a36fbecce7f7facbe56c1bb
YMnooneJM <demraldo12@gmail.com>
2025-07-03 10:50:40 +0100

    Wrap RouterProvider in React.StrictMode for improved development warnings

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -23,5 +24,7 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
